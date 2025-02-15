77e9e0434467203837d588c08ff6ec53e62c5465
Shreyas Jadhav <sshreyasjadhav2021@gmail.com>
2025-02-15 21:58:06 +0530

    Add files via upload

---
+++ 1st.java
@@ -0,0 +1,15 @@
+class ccc
+{
+	public static void main(String args[])
+	{
+		int a=10_0000;
+		String b="ABC";
+		
+		System.out.println("Hello Welcome="+a);
+		System.out.println("Hello Welcome="+b);
+		
+		
+	
+	}
+
+}
